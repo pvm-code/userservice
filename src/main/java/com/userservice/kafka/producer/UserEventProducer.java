4091a9fc717a0e5678f015fa79a3fd9f301333a9
prashant <pvm.code@gmail.com>
2026-08-06 03:11:08 +0530

    prometheus counter metrics added

--- src/main/java/com/userservice/kafka/producer/UserEventProducer.java
+++ src/main/java/com/userservice/kafka/producer/UserEventProducer.java
@@ -28,26 +28,36 @@ public class UserEventProducer {
 		this.metricsService=metricsService;
 
 	}
-
 	public void publishUserRegistered(UserRegisteredEvent event) {
-		
-		try {
-			String json = objectMapper.writeValueAsString(event);
 
-			kafkaTemplate.send(
-					
-					KafkaTopicConfig.USER_REGISTERED_TOPIC,
-					event.getId().toString(),
-					json
-					
-					
-					);
-			metricsService.incrementKafkaProduced();
-		} catch (JsonProcessingException e) {
-		    e.printStackTrace();
-		    throw new RuntimeException("failed to serialize event", e);
-		}
-		
+	    try {
+
+	        String json = objectMapper.writeValueAsString(event);
+
+	        kafkaTemplate.send(
+	                KafkaTopicConfig.USER_REGISTERED_TOPIC,
+	                event.getId().toString(),
+	                json
+	        ).whenComplete((result, ex) -> {
+
+	            if (ex == null) {
+
+	                System.out.println("Kafka message published successfully");
+
+	                metricsService.incrementKafkaProduced();
+
+	            } else {
+
+	                System.err.println("Kafka publish failed");
+	                ex.printStackTrace();
+
+	            }
+
+	        });
+
+	    } catch (Exception e) {
+	        throw new RuntimeException("Failed to publish event", e);
+	    }
 	}
 	
 }
--- src/main/java/com/userservice/service/UserService.java
+++ src/main/java/com/userservice/service/UserService.java
@@ -58,7 +58,7 @@ public class UserService {
 				
 				
 				);
-		
+		System.out.println("Calling Kafka producer...");
 		
 		userEventProducer.publishUserRegistered(event);
 		
